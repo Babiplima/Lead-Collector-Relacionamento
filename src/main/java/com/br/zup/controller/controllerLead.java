449d160661dd6a81e17c0bca1d858d324cf59848
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-18 14:20:24 -0300

    Modelar Classe Controller

--- src/main/java/com/br/zup/controller/controllerLead.java
+++ src/main/java/com/br/zup/controller/controllerLead.java
@@ -1,4 +1,18 @@
 package com.br.zup.controller;
 
+import com.br.zup.DTO.LeadRepository;
+import com.br.zup.Service.Service;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lead")
 public class controllerLead {
+
+    @Autowired
+    private Service service;
+    @Autowired
+    private ModelMapper modelMapper;
 }
