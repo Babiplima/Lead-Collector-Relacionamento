e193c19ec5a91fedcecb88123eae9230d4a66020
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-18 14:26:47 -0300

    Modelar Metodo ByLeadNome em Classe Inteface Repositoy

--- src/main/java/com/br/zup/DTO/LeadRepository.java
+++ src/main/java/com/br/zup/DTO/LeadRepository.java
@@ -3,4 +3,5 @@ package com.br.zup.DTO;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LeadRepository extends CrudRepository<Lead, String> {
+    boolean existsByLeadNome(String nome);
 }
