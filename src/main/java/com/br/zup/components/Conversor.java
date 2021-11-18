cc2adf36a6e17c4dc78a27473007b860d5a08d4a
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-18 14:09:29 -0300

    Modelar Classe Conversor

--- src/main/java/com/br/zup/components/Conversor.java
+++ src/main/java/com/br/zup/components/Conversor.java
@@ -1,4 +1,10 @@
 package com.br.zup.components;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Conversor {
+
+  public ModelMapper modelMapper(){ return new ModelMapper()}
 }
