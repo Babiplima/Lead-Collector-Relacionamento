fead624edc219db0ec6451949e0752beb77ea121
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-18 12:05:08 -0300

    Modelar Classe Lead

--- src/main/java/com/br/zup/DTO/Lead.java
+++ src/main/java/com/br/zup/DTO/Lead.java
@@ -1,4 +1,55 @@
 package com.br.zup.DTO;
 
+import java.util.List;
+
 public class Lead {
+
+    private String email;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private List<ProdutoDTO> produtos;
+
+    public Lead() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<ProdutoDTO> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ProdutoDTO> produtos) {
+        this.produtos = produtos;
+    }
 }
